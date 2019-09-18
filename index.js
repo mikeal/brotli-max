5b3b1ec45bf56f79f23bde8cc00ab390f713dfa2
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-09-18 00:47:36 +0000

    feat: support buffer instances

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const zlib = require('zlib')
 const fs = require('fs')
+const { stat, writeFile } = fs.promises
 
 const main = async (input, output) => {
+  let size
+  if (Buffer.isBuffer(input)) {
+    size = input.length
+  } else if (typeof input === 'string') {
+    size = (await stat(input)).size
+  } else {
+    throw new Error('Unknown input type')
+  }
+
   const stream = zlib.createBrotliCompress({
     params: {
       [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
       [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-      [zlib.constants.BROTLI_PARAM_SIZE_HINT]: fs.statSync(input).size
+      [zlib.constants.BROTLI_PARAM_SIZE_HINT]: size
     }
   })
-  const o = fs.createReadStream(input).pipe(stream).pipe(fs.createWriteStream(output))
-  return new Promise(resolve => o.on('close', resolve))
+  if (typeof input === 'string') {
+    const o = fs.createReadStream(input).pipe(stream).pipe(fs.createWriteStream(output))
+    return new Promise(resolve => o.on('close', resolve))
+  }
+  return writeFile(output, input)
 }
 
 module.exports = main
--- test.js
+++ test.js
@@ -11,5 +11,16 @@ const file = __filename
 const run = async () => {
   await main(file, file + '.br')
   assert.ok(fs.statSync(file + '.br').size)
+  const buff = fs.readFileSync(file)
+  await main(buff, file + '2.br')
+  assert.ok(fs.statSync(file + '2.br').size)
+  try {
+    await main(null, null)
+    throw new Error('Missed failure')
+  } catch (e) {
+    assert.ok(e.message === 'Unknown input type')
+  }
+  fs.unlinkSync(file + '.br')
+  fs.unlinkSync(file + '2.br')
 }
 run()
